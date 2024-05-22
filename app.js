999cf70fa9a4754f1f70b08380dd04f8b2b6ecb3
thisiskeshavyadavv@gmail.com <Keshav yadav>
2024-05-22 18:50:38 +0530

    adding sepration of concerns

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-
 const cookieParser = require("cookie-parser");
 const path = require('path');
+const db = require("./config/mongoose-connection");
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
